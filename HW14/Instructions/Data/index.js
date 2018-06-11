771b839cdcc97d4f855eec8ce52cc1cfe0ce95b6
tfathinejad <35054702+tfathinejad@users.noreply.github.com>
2018-06-11 09:11:40 -0700

    Add files via upload

--- HW14/Instructions/Data/index.js
+++ HW14/Instructions/Data/index.js
@@ -1,6 +1,6 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $dateInput = document.querySelector("#datetime");
+var $datetimeInput = document.querySelector("#datetime");
 var $cityInput = document.querySelector("#city");
 var $stateInput = document.querySelector("#state");
 var $countryInput = document.querySelector("#country");
@@ -19,36 +19,70 @@ function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredData.length; i++) {
     // Get get the current address object and its fields
-    var address = filteredData[i];
-    var fields = Object.keys(address);
+    var info = filteredData[i];
+    var fields = Object.keys(info);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
       // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
+      $cell.innerText = info[field];
     }
+    // console.log(info)
   }
 }
 
 function handleSearchButtonClick() {
   // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterDate = $dateInput.value;
-  var filterCity = $cityInput.value.trim().toLowerCase;
-  var filterState = $stateInput.value.trim().toLowerCase();
-  var filterCountry = $countryInput.value.trim().toLowerCase();
-  var filterShape = $shapeInput.value.trim().toLowerCase;
 
+  var filterDate = $datetimeInput.value;
+  // var filterCity = $cityInput.value.trim().toLowerCase;
+  // var filterState = $stateInput.value.trim().toLowerCase();
+  // var filterCountry = $countryInput.value.trim().toLowerCase();
+  // var filterShape = $shapeInput.value.trim().toLowerCase;
+
+
+  // console.log(filterDate)
   // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-  filteredData = data.filter(function(address) {
-  //   var addressDate = address.datetime.toLowerCase();
-  //   return addressDate === filterDate;
-  // });
-    var addressState = address.state.toLowerCase();
-
+  // infox is chicken
+  filteredData = dataSet.filter(function(infox) {
     // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-    return addressState === filterState;
+    // console.log(infox)
+    // false, 0, '', null, undefined ====> false
+    if (filterDate && filterDate != infox.datetime) {
+      // console.log('failed on dat);
+      return false;
+    }
+    //   else {
+    //   console.log('passed date');
+    // }
+
+    // console.log('=== filter date ===');
+    // console.log(filterDate);
+    // console.log(infox.date.trim().toLowerCase());
+  
+
+    // if (filterCity && filterCity != infox.city.trim().toLowerCase()) {
+    //   console.log(filterCity)
+    //   return false;
+    // }
+
+    // if (filterState && filterState != infox.state.trim().toLowerCase()) {
+    //   return false;
+    // }
+
+    // if (filterCountry && filterCountry != infox.country.trim().toLowerCase()) {
+    //   return false;
+    // }
+
+
+    // if (filterShape && filterShape != infox.shape.trim().toLowerCase()) {
+    //   return false;
+    // }
+
+    // console.log(filteredData)
+    return true;
   });
 
   // console.log(filteredAddresses)
